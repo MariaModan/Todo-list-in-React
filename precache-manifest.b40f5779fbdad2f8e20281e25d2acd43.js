91152947b53a75028f262aea5d6662b45afaeeee
Maria <maria.modan95@gmail.com>
2019-07-21 11:45:34 +0300

    first commit

---
+++ precache-manifest.b40f5779fbdad2f8e20281e25d2acd43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d71129f4ddee5faa6e9cda7bc8ccfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be00cb025797d8007949",
+    "url": "/static/css/main.fa871d13.chunk.css"
+  },
+  {
+    "revision": "c7404376805dbe96ffe4",
+    "url": "/static/js/2.03950753.chunk.js"
+  },
+  {
+    "revision": "be00cb025797d8007949",
+    "url": "/static/js/main.c11678bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
